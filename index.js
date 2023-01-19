224f793ed919eaa7554464a7c04c0ae72650d3d9
JorgeMioska <quispechilojorge@gmail.com>
2023-01-18 22:19:51 -0500

    names displayed + styles

--- index.js
+++ index.js
@@ -4,5 +4,14 @@ const SECURITY_HEADERS = {
     authorization: "token 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"
   }
 };
-
-fetch(API_URL, SECURITY_HEADERS);
+const rootElement = document.getElementById('root');
+rootElement.innerText = 'Loading...';
+// const responsivePromise=fetch(API_URL, SECURITY_HEADERS)
+fetch(API_URL)
+  .then(response => response.json())
+  .then(file => {
+    const fighters = JSON.parse(atob(file.content));
+    const names = fighters.map(it => it.name);
+    const namesStr = names.join('\n'); 
+    rootElement.innerText = namesStr;
+  });
