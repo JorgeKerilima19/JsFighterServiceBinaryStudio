19124c655be50c229bf42bef67f210e73c15ddf8
JorgeMioska <quispechilojorge@gmail.com>
2023-01-18 22:30:07 -0500

    fecth error

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-const API_URL = 'https://api.github.com/repos/oleksandr-danylchenko/street-fighter/contents/resources/api/fighters.json';
-const SECURITY_HEADERS = {
-  headers: {
-    authorization: "token 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"
-  }
-};
+const API_URL = 'https://api.github.com***/repos/oleksandr-danylchenko/street-fighter/contents/resources/api/fighters.json';
+// const SECURITY_HEADERS = {
+//   headers: {
+//     authorization: "token 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"
+//   }
+// };
 const rootElement = document.getElementById('root');
 rootElement.innerText = 'Loading...';
 // const responsivePromise=fetch(API_URL, SECURITY_HEADERS)
 fetch(API_URL)
-  .then(response => response.json())
-  .then(file => {
-    const fighters = JSON.parse(atob(file.content));
-    const names = fighters.map(it => it.name);
-    const namesStr = names.join('\n'); 
-    rootElement.innerText = namesStr;
+.then(
+    response => response.json(),
+  )
+  .catch(error => {
+    console.warn(error);
+    rootElement.innerText = 'Failed to load data';
   });
+//   .then(file => {
+//     const fighters = JSON.parse(atob(file.content));
+//     const names = fighters.map(it => it.name);
+//     const namesStr = names.join('\n'); 
+//     rootElement.innerText = namesStr;
+//   });
